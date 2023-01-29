1ffed7ff3c35f193a28c9682f6901c7c7f9673db
olive-su <1466su@naver.com>
2023-01-30 01:02:35 +0900

    75. 조건부 내용 출력하기

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,18 +7,29 @@ import './Expenses.css';
 const Expenses = props => {
     const [filteredYear, setFilteredYear] = useState('2020');
     const filterChangeHandler = selectedYear => {
-        console.log('Expenses.js');
         console.log(selectedYear);
         setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p> No.expense found. </p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />);
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 {/* 아래와 같은 하드 코딩을 expense로 포매팅된 값으로 바꿔준다. */}
-                {props.items.map(expense => (
-                    <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
-                ))}
+                {/* {props.items.map(expense => (
+                    <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+                ))} */}
+                {expensesContent}
                 {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
                 <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
