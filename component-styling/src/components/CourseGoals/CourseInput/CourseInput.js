139413d5082d0891d558254dfde0eac017486114
olive-su <1466su@naver.com>
2023-01-30 20:09:07 +0900

    90. css 모듈을 사용한 동적 스타일

--- component-styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ component-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-// import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
-    const FormControl = styled.div`
-        margin: 0.5rem 0;
+    // const FormControl = styled.div`
+    //     margin: 0.5rem 0;
 
-        $ label {
-            font-weight: bold;
-            display: block;
-            margin-bottom: 0.5rem;
-            color: ${props => (props.invalid ? 'red' : 'black')};
-        }
+    //     $ label {
+    //         font-weight: bold;
+    //         display: block;
+    //         margin-bottom: 0.5rem;
+    //         color: ${props => (props.invalid ? 'red' : 'black')};
+    //     }
 
-        $ input {
-            display: block;
-            width: 100%;
-            border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-            background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-            font: inherit;
-            line-height: 1.5rem;
-            padding: 0 0.25rem;
-        }
+    //     $ input {
+    //         display: block;
+    //         width: 100%;
+    //         border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    //         background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+    //         font: inherit;
+    //         line-height: 1.5rem;
+    //         padding: 0 0.25rem;
+    //     }
 
-        $ input:focus {
-            outline: none;
-            background: #fad0ec;
-            border-color: #8b005d;
-        }
-    `;
+    //     $ input:focus {
+    //         outline: none;
+    //         background: #fad0ec;
+    //         border-color: #8b005d;
+    //     }
+    // `;
 
     const [enteredValue, setEnteredValue] = useState('');
     const [isValid, setIsValid] = useState(true);
@@ -55,11 +55,11 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl invalid={isValid}>
+            <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
                 {/* styled component에 invalid를 넘겨준다. */}
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler} />
-            </FormControl>
+            </div>
             <Button type="submit">Add Goal</Button>
         </form>
     );
